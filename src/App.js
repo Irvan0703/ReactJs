25e4a1846bf2ef5a1365d20463be027fb81d3b2f
Irvan0703 <muhamadirfanulhadi@gmail.com>
2022-04-07 05:40:18 +0700

    Tugas Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import Routing from './Materi/Routing';
-import TugasStyling from "./Materi/TugasStyling";
-import TaskHook from "./Materi/TaskHook";
-import FormValidation from "./Materi/FormValidation";
-
+import Redux from "./Materi/Redux";
 
 
 function App() {
   return (
     <div className="App" >
-      
-      
-      <Router>
-      <Routing/>
-        <Routes >
-          <Route exact path="/" element={<TugasStyling />}/>
-          <Route exact path="/News" element={<TaskHook />}/>
-          <Route path="/Register" element={<FormValidation />} />
-        </Routes>
-      </Router>
+      <Redux/>
     </div>
   );
 }
